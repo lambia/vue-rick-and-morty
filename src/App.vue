4dc6216c67b838cf39636f101ec1496432a2630c
Luca Lambiase <luca.lambia@gmail.com>
2023-11-30 11:40:48 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+
+export default {
+	components: {
+	},
+	data() {
+		return {
+		}
+	}
+}
+</script>
+
+<template>
+	<main>Benvenuto in Rick and Morty</main>
+</template>
+
+<style scoped></style>
