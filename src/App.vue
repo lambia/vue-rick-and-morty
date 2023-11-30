69791065303f16dfacc77bf75f300e69ae307f63
Luca Lambiase <luca.lambia@gmail.com>
2023-11-30 12:58:34 +0100

    state management di base

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <script>
+import AppHeader from "./components/AppHeader.vue"
+import AppFooter from "./components/AppFooter.vue"
 
 export default {
 	components: {
+		AppHeader,
+		AppFooter
 	},
 	data() {
 		return {
+			menuLinks: [
+				"Home",
+				"Chi Siamo",
+				"Contatti"
+			]
 		}
 	}
 }
 </script>
 
 <template>
-	<main>Benvenuto in Rick and Morty</main>
+	<AppHeader title="Benvenuto in Rick and Morty" :links="menuLinks" />
+	<main>
+		Main Placeholder
+	</main>
+	<AppFooter :links="menuLinks" />
 </template>
 
 <style scoped></style>
